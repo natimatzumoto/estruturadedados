210076e7a4f3ed8ee2c87563d50e08c0723f6019
Natália Mari Matzumoto <natalia.matzumoto@unifesp.br>
2025-10-22 14:57:24 -0300

    Update and rename Exercício 1.ssh to Exercício 1.c
    Correções sugeridas pelo professor

--- Exercício 1.c
+++ Exercício 1.c
@@ -1,3 +1,8 @@
+#### Recebe um número inteiro N de valores a serem submetidos e a série de N números. Retorna os valores máximo e mínimo, a média, a mediana, 
+o desvio-padrão (populacional) dos valores. ####
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
